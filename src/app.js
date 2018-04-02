5cade0356ae86b1baa740c62561a7dc7eda2dbbf
Daniel Hilst Selli <daniel@gmail.com>
2018-04-02 19:41:42 -0300

    Add files

---
+++ src/app.js
@@ -0,0 +1,1 @@
+export default function helloworld() { console.log("Hello World"); };
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+module.exports = {
+    entry: "./src/app.js",
+    output: {
+        filename: "helloworld.js",
+        path: path.resolve(__dirname, "dist"),
+        library: "helloworld",
+        libraryTarget: "umd",
+    },
+    mode: process.env.NODE_ENV || "production",
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            }
+        ]
+    }
+}
